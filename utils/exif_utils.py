6bd8013ff7119fedbd81b0ef14619b210c352c07
Konstantin Belotelov <33226823+CostiaB@users.noreply.github.com>
2024-10-29 09:51:24 -0300

    No coodinates fix

--- utils/exif_utils.py
+++ utils/exif_utils.py
@@ -13,12 +13,12 @@ def get_exif_data(image):
                     gps_data = {}
                     for gps_tag in value:
                         gps_tag_name = GPSTAGS.get(gps_tag, gps_tag)
-                        gps_data[gps_tag_name] = value
+                        gps_data[gps_tag_name] = value[gps_tag]
                     exif_data[tag_name] = gps_data
                 else:
                     exif_data[tag_name] = value
     except Exception as e:
-        print(f"Error getting EXIF data from {image}: {e} ")
+        print(f"Error getting EXIF data from {image}: {e}")
     return exif_data
 
 def get_coordinates(gps_info):
@@ -26,17 +26,23 @@ def get_coordinates(gps_info):
         return None
 
     def to_degrees(value):
+        if not value or len(value) < 3:
+            return None
         d = float(value[0])
         m = float(value[1])
         s = float(value[2])
         return d + (m / 60.0) + (s / 3600.0)
 
-    lat = to_degrees(gps_info.get("GPSLatitude", [0, 0, 0]))
-    if gps_info.get("GPSLatitudeRef") == 'S':
+    lat = to_degrees(gps_info.get("GPSLatitude"))
+    if lat is not None and gps_info.get("GPSLatitudeRef") == 'S':
         lat = -lat
 
-    lon = to_degrees(gps_info.get("GPSLongitude", [0, 0, 0]))
-    if gps_info.get("GPSLongitudeRef") == 'W':
+    lon = to_degrees(gps_info.get("GPSLongitude"))
+    if lon is not None and gps_info.get("GPSLongitudeRef") == 'W':
         lon = -lon
 
-    return (lat, lon)
+    if lat is not None and lon is not None:
+        return (lat, lon)
+    else:
+        return None
+
